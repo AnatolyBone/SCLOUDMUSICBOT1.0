33fe675835ab7176c981c9e4f1c720d5f248ac20
AnatolyBone <80332097+AnatolyBone@users.noreply.github.com>
2025-08-20 05:24:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ import {
     pool, supabase, getUserById, resetDailyStats, getAllUsers, getPaginatedUsers, 
     getReferralSourcesStats, getDownloadsByDate, getRegistrationsByDate, 
     getActiveUsersByDate, getExpiringUsers, setPremium, updateUserField, 
-    getLatestReviews, getUserActivityByDayHour, getDownloadsByUserId, getReferralsByUserId, getCachedTracksCount
+    getLatestReviews, getUserActivityByDayHour, getDownloadsByUserId, getReferralsByUserId, getCachedTracksCount, getActiveFreeUsers, getActivePremiumUsers
 } from './db.js';
 import { bot } from './bot.js';
 import redisService from './services/redisClient.js';
