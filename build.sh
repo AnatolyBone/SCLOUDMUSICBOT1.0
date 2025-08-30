f38ee9510fe17502fcf7c43776fd3e673997556c
AnatolyBone <80332097+AnatolyBone@users.noreply.github.com>
2025-08-30 09:05:50 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,14 +2,14 @@
 # exit on error
 set -o errexit
 
-# Устанавливаем системные зависимости
+# Устанавливаем системные зависимости с правами sudo
 echo ">>> Installing system dependencies (ffmpeg)..."
-apt-get update && apt-get install -y ffmpeg
+sudo apt-get update && sudo apt-get install -y ffmpeg
 
 # Устанавливаем Python зависимости ИЗ ФАЙЛА requirements.txt
 echo ">>> Installing Python packages from requirements.txt..."
 pip install --upgrade pip
-pip install -r requirements.txt # <--- ВОТ ГЛАВНОЕ ИСПРАВЛЕНИЕ
+pip install -r requirements.txt
 
 # Устанавливаем npm зависимости
 echo ">>> Installing Node.js packages..."
