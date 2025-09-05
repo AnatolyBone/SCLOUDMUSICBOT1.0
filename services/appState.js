ea3c3d8cbc3ef521be877c0fd3b08742a3ff39e6
AnatolyBone <80332097+AnatolyBone@users.noreply.github.com>
2025-09-05 12:45:41 +0300

    Create appState.js

---
+++ services/appState.js
@@ -0,0 +1,21 @@
+// services/appState.js
+
+// Флаг, который показывает, что приложение в процессе принудительного завершения работы.
+export let isShuttingDown = false;
+
+// Флаг, который показывает, что включен ручной режим обслуживания.
+export let isMaintenanceMode = false;
+
+// Функция для установки флага принудительного завершения.
+export function setShuttingDown() {
+  if (!isShuttingDown) {
+    console.log('[Shutdown] Установлен флаг завершения работы. Новые задачи не принимаются.');
+    isShuttingDown = true;
+  }
+}
+
+// Функция для ручного управления режимом обслуживания.
+export function setMaintenanceMode(state) {
+    isMaintenanceMode = state;
+    console.log(`[Maintenance] Режим обслуживания ${state ? 'ВКЛЮЧЕН' : 'ВЫКЛЮЧЕН'}.`);
+}
